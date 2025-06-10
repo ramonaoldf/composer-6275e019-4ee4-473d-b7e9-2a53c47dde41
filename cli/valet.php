8e511bd0226913fc68e88b637c107756b178eab0
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-10 03:13:19 -0300

    Vesion: 001.001.012.000

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.11';
+$version = '1.1.12';
 
 $app = new Application('Laravel Valet', $version);
 
@@ -153,7 +153,7 @@ $app->command('which', function () {
     } else {
         warning('Valet could not determine which driver to use for this site.');
     }
-})->descriptions('Determine which Valet driver serves the current workign directory');
+})->descriptions('Determine which Valet driver serves the current working directory');
 
 /**
  * Stream all of the logs for all sites.
